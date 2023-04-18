5d879149b84b2529da6a3ecc604b6befbeb9f892
AlexVoyt <44990317+AlexVoyt@users.noreply.github.com>
2023-04-18 15:14:24 +1000

    Make qt5 optional dependency

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
-from setuptools import setup, find_packages
-from os.path import join, dirname
-from setuptools import Command
 import os
+from os.path import join, dirname
+from setuptools import setup, find_packages
+
 import k1921vkx_flasher
+
 setup(
     name='k1921vkx_flasher',
     version=k1921vkx_flasher.__version__,
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     install_requires=[
-        'pyqt5',
         'pyserial'
     ],
+    extras_require={
+        'GUI': [
+            'pyqt5'
+        ]
+    },
     entry_points={
         'console_scripts':
             ['k1921vkx_flasher = k1921vkx_flasher.__main__:main']
